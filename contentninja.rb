258cf3b913b5921051f1e642bd89e79397b9aa2f
contentninjaservices <rpr@9it.de>
2015-04-17 15:44:38 +0200

    change sha digest dfdb0e708101709bea3b2d3c764f7b0d6703a85b399e8f8e38c4b17f9dd91b1e

--- contentninja.rb
+++ contentninja.rb
@@ -3,7 +3,7 @@ class Contentninja < Formula
   homepage "http://contentninja.services"
   url "https://github.com/contentninjaservices/contentninja/archive/master.zip"
   version "0.8.5"
-  sha256 "3c2e1bab71d38ef2656a6990892b42bb6e40719413073c94da0e1b81d9de9f99"
+  sha256 "dfdb0e708101709bea3b2d3c764f7b0d6703a85b399e8f8e38c4b17f9dd91b1e"
 
   def install
     system "make install" # if this fails, try separate make/make install steps
