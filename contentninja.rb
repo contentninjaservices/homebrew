ba53ce57dc430173503f450ffcced09e614492f5
contentninjaservices <rpr@9it.de>
2015-04-17 15:40:20 +0200

    change back to sha256

--- contentninja.rb
+++ contentninja.rb
@@ -3,8 +3,7 @@ class Contentninja < Formula
   homepage "http://contentninja.services"
   url "https://github.com/contentninjaservices/contentninja/archive/master.zip"
   version "0.8.5"
-  # sha256 "3c2e1bab71d38ef2656a6990892b42bb6e40719413073c94da0e1b81d9de9f99"
-	md5 "9c5b6233bd69598d1c457ff2f70856fc"
+  sha256 "3c2e1bab71d38ef2656a6990892b42bb6e40719413073c94da0e1b81d9de9f99"
 
   def install
     system "make install" # if this fails, try separate make/make install steps
